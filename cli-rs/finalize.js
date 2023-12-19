8be7759b7af891ff9cec8dad05586de3a41e8d57
AHQ Miness <ahqsecret@gmail.com>
2023-12-19 22:26:10 +0530

    Darwin Support

--- cli-rs/finalize.js
+++ cli-rs/finalize.js
@@ -7,10 +7,13 @@ let json = JSON.parse(readFileSync(packageJson));
 
 if (process.platform == "win32") {
   json.name = "@ahqstore/cli-rs-win32";
-} else {
+} else if (process.platform == "linux") {
   json.name = "@ahqstore/cli-rs-linux";
+} else if (process.platform == "darwin") {
+  json.name = "@ahqstore/cli-rs-darwin";
 }
 
+json.description = `AHQ Store CLI Binaries for ${process.platform}-${process.arch}`;
 json.repository = {
   type: "git",
   url: "git+https://github.com/ahqstore/cli.git",
