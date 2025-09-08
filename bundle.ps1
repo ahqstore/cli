b95037b8a29dde05d2a0828368edf30f4b670e8e
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-09-08 15:42:13 +0530

    Final fix

--- bundle.ps1
+++ bundle.ps1
@@ -1,7 +1,11 @@
 mkdir dist
 
-Copy-Item "./target/$env:TARGET/release/ahqstore.exe" "./dist/ahqstore.exe"
-Copy-Item "./target/$env:TARGET/release/ahqstore.exe" "./dist/ahqstore_cli_rs.exe"
+Copy-Item "./target/$env:TARGET/release/ahqstore.exe" "./dist/ahqstore.exe" -ErrorAction Continue
+Copy-Item "./target/$env:TARGET/release/ahqstore.exe" "./dist/ahqstore_cli_rs.exe" -ErrorAction Continue
+
+Copy-Item "./target/$env:TARGET/release/ahqstore" "./dist/ahqstore" -ErrorAction Continue
+Copy-Item "./target/$env:TARGET/release/ahqstore" "./dist/ahqstore_cli_rs" -ErrorAction Continue
+
 compress-archive ./dist/* ./ahqstore_cli_rs-$env:TARGET
 
 Copy-Item "./target/$env:TARGET/release/ahqstore_cli_rs.dll" "./ahqstore_cli_rs-$env:TARGET.dll" -ErrorAction Continue
